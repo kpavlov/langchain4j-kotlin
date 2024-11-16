3df6628364d6c0eed54f4388efcd819d2128fe9d
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2024-11-16 18:46:19 +0200

    Add dynamic instance creation for prompt components
    Introduced methods to load and instantiate `PromptTemplateSource` and `TemplateRenderer` using configuration properties. Enhanced the configuration class to support dynamic instance creation by class name, which improves flexibility and maintainability.

--- langchain4j-kotlin/src/main/kotlin/me/kpavlov/langchain4j/kotlin/Configuration.kt
+++ langchain4j-kotlin/src/main/kotlin/me/kpavlov/langchain4j/kotlin/Configuration.kt
@@ -1,9 +1,18 @@
 package me.kpavlov.langchain4j.kotlin
 
+import me.kpavlov.langchain4j.kotlin.prompt.PromptTemplateSource
+import me.kpavlov.langchain4j.kotlin.prompt.TemplateRenderer
 import java.util.Properties
 
 object Configuration {
     val properties: Properties = loadProperties()
+
+    operator fun get(key: String): String = properties.getProperty(key)
+
+    val promptTemplateSource: PromptTemplateSource =
+        createInstanceByName(this["prompt.template.source"])
+    val promptTemplateRenderer: TemplateRenderer =
+        createInstanceByName(this["prompt.template.renderer"])
 }
 
 private fun loadProperties(fileName: String = "langchain4j-kotlin.properties"): Properties {
@@ -17,3 +26,14 @@ private fun loadProperties(fileName: String = "langchain4j-kotlin.properties"): 
     }
     return properties
 }
+
+@Suppress("UNCHECKED_CAST", "TooGenericExceptionCaught")
+private fun <T> createInstanceByName(className: String): T =
+    try {
+        // Get the class object by name
+        val clazz = Class.forName(className)
+        // Create an instance of the class
+        clazz.getDeclaredConstructor().newInstance() as T
+    } catch (e: Exception) {
+        throw IllegalArgumentException("Can't create $className", e)
+    }
