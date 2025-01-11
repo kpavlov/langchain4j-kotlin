359e4a2f0eea26b9a16ab8f90afef64da91e5ee2
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2025-01-11 14:42:54 +0200

    Refactor and enhance template system handling
    Made `SystemMessageProvider` a functional interface and added a default implementation for `apply`. Introduced new Mustache templates for system and user prompts. Updated documentation and examples to simplify prompt template handling. Configured Maven for Kotlin and Java interop while adding proper compiler settings. Added Java sample demonstrating template usage.

---
+++ samples/src/main/java/me/kpavlov/langchain4j/kotlin/samples/ServiceWithTemplateSourceJavaExample.java
@@ -0,0 +1,46 @@
+package me.kpavlov.langchain4j.kotlin.samples;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.mock.ChatModelMock;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.UserName;
+import dev.langchain4j.service.V;
+import me.kpavlov.langchain4j.kotlin.service.TemplateSystemMessageProvider;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class ServiceWithTemplateSourceJavaExample {
+
+  // Use for demo purposes
+  private static final ChatLanguageModel model = new ChatModelMock("Hello");
+  private static final Logger LOGGER = getLogger(ServiceWithTemplateSourceJavaExample.class);
+
+  private static final String PROMPT_TEMPLATE_PATH = "prompts/ServiceWithTemplateSourceJavaExample";
+
+  private interface Assistant {
+    @UserMessage(PROMPT_TEMPLATE_PATH + "/default-user-prompt.mustache")
+    String askQuestion(
+      @UserName String userName,
+      @V("message") String question
+    );
+  }
+
+  public static void main(String[] args) {
+
+    final var systemMessageProvider = new TemplateSystemMessageProvider(
+      PROMPT_TEMPLATE_PATH + "/default-system-prompt.mustache"
+    );
+
+    final Assistant assistant = AiServices.builder(Assistant.class)
+      .systemMessageProvider(systemMessageProvider)
+      .chatLanguageModel(model)
+      .build();
+
+    String response = assistant.askQuestion("My friend", "How are you?");
+    LOGGER.info("AI Response: {}", response);
+  }
+}
+
+
