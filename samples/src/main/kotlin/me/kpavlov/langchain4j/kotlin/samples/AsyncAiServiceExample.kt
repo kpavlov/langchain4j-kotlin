cfb76a0726e06c05d53e5e48f9ca8688129ecb1f
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-05-17 12:14:57 +0300

    Fix samples (#146)
    Updating dependencies in `pom.xml`, refactoring Kotlin examples to
    remove redundant `ChatModel` initialization, and centralizing the
    `ChatModel` configuration in a shared `Environment.kt` file.

--- samples/src/main/kotlin/me/kpavlov/langchain4j/kotlin/samples/AsyncAiServiceExample.kt
+++ samples/src/main/kotlin/me/kpavlov/langchain4j/kotlin/samples/AsyncAiServiceExample.kt
@@ -1,23 +1,20 @@
 package me.kpavlov.langchain4j.kotlin.samples
 
 import dev.langchain4j.model.chat.ChatModel
-import dev.langchain4j.model.openai.OpenAiChatModel
 import dev.langchain4j.service.UserMessage
 import kotlinx.coroutines.runBlocking
 import me.kpavlov.langchain4j.kotlin.service.AsyncAiServicesFactory
 import me.kpavlov.langchain4j.kotlin.service.createAiService
 
-private val model: ChatModel = OpenAiChatModel.builder()
-    .modelName("gpt-4o-mini")
-    .apiKey(testEnv["OPENAI_API_KEY"])
-    .build()
 
 fun interface AsyncAssistant {
     suspend fun askQuestion(@UserMessage question: String): String
 }
 
-fun main() {
-    runBlocking {
+class AsyncAiServiceExample(
+    private val model: ChatModel
+) {
+    suspend fun callAiService(): String {
         val assistant = createAiService(
             serviceClass = AsyncAssistant::class.java,
             factory = AsyncAiServicesFactory(),
@@ -25,9 +22,15 @@ fun main() {
             .systemMessageProvider { "You are a helpful software engineer" }
             .build()
 
-        val response = assistant.askQuestion(
+        return assistant.askQuestion(
             "What's new in Kotlin/AI space in one sentence?"
         )
+    }
+}
+
+fun main() {
+    runBlocking {
+        val response = AsyncAiServiceExample(model).callAiService()
         println("AI Answer: \"$response\"")
     }
 }
