7d024789deacd2bc670b4480661168ddb66649fe
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-05-17 11:39:34 +0300

    Refactor AsyncAiServiceExample for improved clarity and usage

--- samples/src/main/kotlin/me/kpavlov/langchain4j/kotlin/samples/AsyncAiServiceExample.kt
+++ samples/src/main/kotlin/me/kpavlov/langchain4j/kotlin/samples/AsyncAiServiceExample.kt
@@ -3,40 +3,31 @@ package me.kpavlov.langchain4j.kotlin.samples
 import dev.langchain4j.model.chat.ChatModel
 import dev.langchain4j.model.openai.OpenAiChatModel
 import dev.langchain4j.service.UserMessage
+import kotlinx.coroutines.runBlocking
 import me.kpavlov.langchain4j.kotlin.service.AsyncAiServicesFactory
 import me.kpavlov.langchain4j.kotlin.service.createAiService
 
-class AsyncAiServiceExample(
-    private val model: ChatModel =
-        OpenAiChatModel
-            .builder()
-            .modelName("gpt-4o-mini")
-            .apiKey(testEnv.get("OPENAI_API_KEY", "demo"))
-            .temperature(0.0)
-            .build(),
-) {
-    fun interface AsyncAssistant {
-        suspend fun askQuestion(
-            @UserMessage question: String,
-        ): String
-    }
+private val model: ChatModel = OpenAiChatModel.builder()
+    .modelName("gpt-4o-mini")
+    .apiKey(testEnv["OPENAI_API_KEY"])
+    .build()
 
-    suspend fun callAiService(): String {
-        val assistant =
-            createAiService(
-                serviceClass = AsyncAssistant::class.java,
-                factory = AsyncAiServicesFactory(),
-            ).chatModel(model)
-                .systemMessageProvider { "You are a helpful assistant" }
-                .build()
-
-        val response = assistant.askQuestion("How are you?")
-        println("AI Answer: \"$response\"")
-        return response
-    }
+fun interface AsyncAssistant {
+    suspend fun askQuestion(@UserMessage question: String): String
 }
 
-// fun main() =
-//    runBlocking {
-//        AsyncAiServiceExample().callAiService()
-//    }
+fun main() {
+    runBlocking {
+        val assistant = createAiService(
+            serviceClass = AsyncAssistant::class.java,
+            factory = AsyncAiServicesFactory(),
+        ).chatModel(model)
+            .systemMessageProvider { "You are a helpful software engineer" }
+            .build()
+
+        val response = assistant.askQuestion(
+            "What's new in Kotlin/AI space in one sentence?"
+        )
+        println("AI Answer: \"$response\"")
+    }
+}
